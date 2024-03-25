d671ac39f75ee2ab66264a53abf00d209cf8b9fb
Pavel-go-basik <158614487+Pavel-go-basik@users.noreply.github.com>
2024-03-26 02:46:18 +0300

    Update precode.go

--- precode.go
+++ precode.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 	"strings"
 	"testing"
-
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -47,33 +47,40 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(answer))
 }
+
 func TestMainHandlerWhenOk(t *testing.T) {
 	req := httptest.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
+	
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
-	assert.Equal(t, responseRecorder.Code, http.StatusOK)
+	
+	require.Equal(t, responseRecorder.Code, http.StatusOK)
 	assert.NotNil(t, responseRecorder.Body)
 }
 
 func TestMainHandlerErrorCity(t *testing.T) {
 	req := httptest.NewRequest("GET", "/cafe?count=2&city=spb", nil)
+	
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
-	assert.Equal(t, responseRecorder.Code, http.StatusBadRequest)
-	expected := `wrong city value`
-	assert.Equal(t, responseRecorder.Body.String(), expected)
-
+	
+	require.Equal(t, responseRecorder.Code, http.StatusBadRequest)
+	assert.Equal(t, responseRecorder.Body.String(), "wrong city value")
 }
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	totalCount := 4
 	req := httptest.NewRequest("GET", "/cafe?count=7&city=moscow", nil)
+	
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
+	
 	body := responseRecorder.Body.String()
 	list := strings.Split(body, ",")
-	assert.Equal(t, len(list), totalCount)
+	
+	require.Equal(t, http.StatusOK, responseRecorder.Code)
+	assert.Len(t, list, totalCount)
 }
